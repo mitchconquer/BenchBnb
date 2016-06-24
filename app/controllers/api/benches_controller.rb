800d6f01116c858e78a3937d434a1daf067bd1d0
mitchconquer <mitch@robotshanghai.com>
2016-06-24 11:28:00 -0700

    Add simple API controller for Bench index and create

---
+++ app/controllers/api/benches_controller.rb
@@ -0,0 +1,19 @@
+class Api::BenchesController < ApplicationController
+
+  def index
+    @benches = Bench.all
+    render :json @benches
+  end
+
+  def create
+    @bench = Bench.new(bench_params)
+    if @bench.save!
+      render :json @bench
+    end
+  end
+
+  private
+  def bench_params
+    params.require(:bench).permit(:description, :lat, :long)
+  end
+end
