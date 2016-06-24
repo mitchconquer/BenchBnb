7f6f46c9d8e43e90d08cf150fbf401a08cf6fcbd
mitchconquer <mitch@robotshanghai.com>
2016-06-24 12:28:16 -0700

    Render app onto page via entry file

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,6 @@
+const React  = require('react');
+const ReactDOM = require('react-dom');
+
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDOM.render(<div>Oh hai!</div>, document.getElementById('content'));
+});
